ea5ff0c6b5ff7c6a0cbb6bea7893674dd1f1b736
Paung <paungcuy15@gmail.com>
2025-08-17 19:42:31 +0700

    pm2 config file

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,28 +1,29 @@
 module.exports = {
-  "apps": [
+  apps: [
     {
-      "name": "laravel-app",
-      "script": "php",
-      "args": "artisan serve --host=0.0.0.0 --port=8000",
-      "cwd": "/home/asfung/Dev/Talker/TServer",
-      "interpreter": "none",
-      "watch": false
+      name: "laravel-app",
+      script: "php",
+      args: "artisan serve --host=0.0.0.0 --port=8000",
+      cwd: ".",
+      interpreter: "none",
+      watch: false
     },
     {
-      "name": "laravel-queue",
-      "script": "php",
-      "args": "artisan queue:work --sleep=3 --tries=3",
-      "cwd": "/home/asfung/Dev/Talker/TServer",
-      "interpreter": "none",
-      "watch": false
+      name: "laravel-queue",
+      script: "php",
+      args: "artisan queue:work --sleep=3 --tries=3",
+      cwd: ".",
+      interpreter: "none",
+      watch: false
     },
     {
-      "name": "laravel-schedule",
-      "script": "php",
-      "args": "artisan schedule:work",
-      "cwd": "/home/asfung/Dev/Talker/TServer",
-      "interpreter": "none",
-      "watch": false
+      name: "laravel-schedule",
+      script: "php",
+      args: "artisan schedule:work",
+      cwd: ".",
+      interpreter: "none",
+      watch: false
     }
   ]
 }
+
